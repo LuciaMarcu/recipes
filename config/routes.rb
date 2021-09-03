7965310308e4e16581a2934d3e73e429412723b0
LuciaMarcu <lucia_sanzianu@yahoo.com>
2021-09-03 15:02:33 +0300

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'  
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -6,7 +7,9 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/all_recipes', to: 'recipes#index'
   post    'add_recipes', to: 'recipes#create'
-  #post    '/signup' , to: 'users#create' 
+  get     '/login'     , to: 'sessions#new'
+  post    '/login'     , to: 'sessions#create'
+  delete  '/logout'    , to: 'sessions#destroy'
   
   resources :users
   resources :recipes
